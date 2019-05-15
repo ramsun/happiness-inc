3c6d642a55ebf46907de2973a7752c9b1d1f01a1
ksan1510 <45074374+ksan1510@users.noreply.github.com>
2019-05-14 21:07:26 -0400

    pushing Rich's maps and UI

---
+++ Site/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicmljaGFyZGt5dSIsImEiOiJjanZlZXA0a3cwbjVmM3lwNnYza2VobTY3In0.dng4QqqBvYvMznCcg5HG2A";
---
+++ Site/static/js/logic.js
@@ -0,0 +1,259 @@
+var map = L.map('map',{minZoom: 2, maxZoom: 8}).setView([30,0], 3);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + API_KEY, {
+    id: 'mapbox.light',
+    noWrap: true
+}).addTo(map);
+
+
+//Function to determine coloring based on happiness score
+function getColor(d) {
+  return d > 7.00 ? '#00ECFF' :
+         d > 6.5  ? '#00FCC3' :
+         d > 6  ? '#00FA77' :
+         d > 5.5  ? '#1BF500' :
+         d > 5  ? '#EDEC00' :
+         d > 4   ? '#EBA400' :
+         d > 3.00   ? '#E85E00' :
+                    '#E61900';
+}
+
+//GeoJson for 2015 Data
+happiness_for_2015 = L.geoJson(world_borders, {
+  
+  style: function (feature) {
+    console.log(feature.properties.ADMIN);
+    //Associate world_borders dataset with the countries in happinessdata_2017
+    var happiness_color = "#808080"
+    for(i=0;i<happiness_data_2015.length;i++){
+      if(feature.properties.ADMIN === happiness_data_2015[i].Country){
+      console.log(happiness_data_2015[i]['Happiness Score'])
+      happiness_color = getColor(happiness_data_2015[i]['Happiness Score'])
+      }
+    }
+    //Fill in all the variables
+    return {
+      fillColor: happiness_color,
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.6
+  };
+  },
+  
+  //onEachFeature
+  onEachFeature: function (feature, layer) {
+    var happiness_data = happiness_data_2015
+    var happiness_score = 0
+    var happiness_rank
+    var flag
+      for(i=0;i<happiness_data.length;i++){
+        if(feature.properties.ADMIN === happiness_data[i].Country){
+        happiness_score = happiness_data[i]['Happiness Score'].toPrecision(3)
+        happiness_rank = happiness_data[i]['Happiness Rank']
+        }
+      }
+      for(i=0;i<flag_data.length;i++){
+        if(feature.properties.ADMIN===flag_data[i].name){
+          console.log(feature.properties.ADMIN, flag_data[i].name, flag_data[i].emoji)
+          flag=flag_data[i].emoji
+        }
+      }
+      layer.bindPopup("<b><font size =\"+1\">"+feature.properties.ADMIN+" </b>"+flag+"</br> Rank: "+happiness_rank+"</font><hr>" +"<p>Happiness Score: "+happiness_score+"</p>"+ "<li style=\"background-color:" + getColor(happiness_score) + "\"></li>");
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          this.closePopup();
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.6
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+   
+      });
+  }
+});
+
+//GeoJson for 2016 Data
+happiness_for_2016 = L.geoJson(world_borders, {
+  
+  style: function (feature) {
+    console.log(feature.properties.ADMIN);
+    //Associate world_borders dataset with the countries in happinessdata_2017
+    var happiness_color = "#808080"
+    for(i=0;i<happiness_data_2016.length;i++){
+      if(feature.properties.ADMIN === happiness_data_2016[i].Country){
+      console.log(happiness_data_2016[i]['Happiness Score'])
+      happiness_color = getColor(happiness_data_2016[i]['Happiness Score'])
+      }
+    }
+    //Fill in all the variables
+    return {
+      fillColor: happiness_color,
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.6
+  };
+  },
+  
+  //onEachFeature
+  onEachFeature: function (feature, layer) {
+    var happiness_data = happiness_data_2016
+    var happiness_score = 0
+    var happiness_rank
+    var flag
+      for(i=0;i<happiness_data.length;i++){
+        if(feature.properties.ADMIN === happiness_data[i].Country){
+        happiness_score = happiness_data[i]['Happiness Score'].toPrecision(3)
+        happiness_rank = happiness_data[i]['Happiness Rank']
+        }
+      }
+      for(i=0;i<flag_data.length;i++){
+        if(feature.properties.ADMIN===flag_data[i].name){
+          console.log(feature.properties.ADMIN, flag_data[i].name, flag_data[i].emoji)
+          flag=flag_data[i].emoji
+        }
+      }
+      layer.bindPopup("<b><font size =\"+1\">"+feature.properties.ADMIN+" </b>"+flag+"</br> Rank: "+happiness_rank+"</font><hr>" +"<p>Happiness Score: "+happiness_score+"</p>"+ "<li style=\"background-color:" + getColor(happiness_score) + "\"></li>");
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          this.closePopup();
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.6
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+   
+      });
+  }
+});
+
+//GeoJson for 2017 Data
+happiness_for_2017 = L.geoJson(world_borders, {
+  
+  style: function (feature) {
+    console.log(feature.properties.ADMIN);
+    //Associate world_borders dataset with the countries in happinessdata_2017
+    var happiness_color = "#808080"
+    for(i=0;i<happiness_data_2017.length;i++){
+      if(feature.properties.ADMIN === happiness_data_2017[i].Country){
+      console.log(happiness_data_2017[i]['Happiness.Score'])
+      happiness_color = getColor(happiness_data_2017[i]['Happiness.Score'])
+      }
+    }
+    //Fill in all the variables
+    return {
+      fillColor: happiness_color,
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.6
+  };
+  },
+  
+  //onEachFeature
+  onEachFeature: function (feature, layer) {
+    var happiness_data = happiness_data_2017
+    var happiness_score = 0
+    var happiness_rank
+    var flag
+      for(i=0;i<happiness_data.length;i++){
+        if(feature.properties.ADMIN === happiness_data[i].Country){
+        happiness_score = happiness_data[i]['Happiness.Score'].toPrecision(3)
+        happiness_rank = happiness_data[i]['Happiness.Rank']
+        }
+      }
+      for(i=0;i<flag_data.length;i++){
+        if(feature.properties.ADMIN===flag_data[i].name){
+          console.log(feature.properties.ADMIN, flag_data[i].name, flag_data[i].emoji)
+          flag=flag_data[i].emoji
+        }
+      }
+      layer.bindPopup("<b><font size =\"+1\">"+feature.properties.ADMIN+" </b>"+flag+"</br> Rank: "+happiness_rank+"</font><hr>" +"<p>Happiness Score: "+happiness_score+"</p>"+ "<li style=\"background-color:" + getColor(happiness_score) + "\"></li>");
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          this.closePopup();
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.6
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+   
+      });
+  }
+}).addTo(map);
+
+// Create Legend display on the bottom right.
+var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var happiness_ranks = [0.00,3,4,5,5.5,6,6.5,7.00];
+    var colors = ["#E61900",'#E85E00','#EBA400','#EDEC00','#1BF500','#00FA77','#00FCC3','#00ECFF']
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h2><center>Happiness Score</center></h2>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + happiness_ranks[0] + "</div>" +
+        "<div class=\"max\">" + happiness_ranks[happiness_ranks.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    happiness_ranks.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(map);
+
+  // Allow for selection by year
+  // configuration to fix map
+  var baseMaps = {
+    "Overlay of 2015 Data" : happiness_for_2015,
+    "Overlay of 2016 Data" : happiness_for_2016,
+    "Overlay of 2017 Data" : happiness_for_2017
+    
+  };
+
+  var overlayMap = {
+  };
+
+  L.control.layers(baseMaps, overlayMap).addTo(map);
